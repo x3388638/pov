998193f3d6ca5729590baade2fb4db1803e3e763
YY <z3388638@gmail.com>
2023-12-16 12:53:32 +0800

    fix: contenful script supports pagination

--- public/contentful.js
+++ public/contentful.js
@@ -3,26 +3,45 @@ const path = require('path')
 
 const { SPACE_ID = '', ACCESS_TOKEN = '' } = process.env
 const FILE_NAME = 'contentfulEntries.json'
+const LIMIT = 800
 
-const exec = (space, accessToken) => {
-  if (!space || !accessToken) {
+const entries = { items: [], includes: { Asset: [] } }
+
+const getEntries = (limit = LIMIT, skip = 0) =>
+  fetch(
+    `https://cdn.contentful.com/spaces/${SPACE_ID}/environments/master/entries?access_token=${ACCESS_TOKEN}&limit=${limit}&skip=${skip}`
+  )
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(`Data fetched - limit: ${limit}; skip: ${skip} `)
+      entries.items.push(...(res?.items || []))
+      entries.includes.Asset.push(...(res?.includes?.Asset || []))
+
+      if (res?.total > limit + skip) {
+        return getEntries(limit, skip + limit)
+      }
+    })
+
+const exec = () => {
+  if (!SPACE_ID || !ACCESS_TOKEN) {
     console.error(
       'Please provide process.env.SPACE_ID and process.env.ACCESS_TOKEN'
     )
     return
   }
 
-  fetch(
-    `https://cdn.contentful.com/spaces/${space}/environments/master/entries?access_token=${accessToken}`
-  )
-    .then((res) => res.json())
-    .then((res) => {
-      fs.writeFileSync(path.resolve(__dirname, FILE_NAME), JSON.stringify(res))
+  getEntries()
+    .then(() => {
+      fs.writeFileSync(
+        path.resolve(__dirname, FILE_NAME),
+        JSON.stringify(entries)
+      )
       console.log(`Contentful data updated in ${FILE_NAME}`)
+      console.log(`Total entry count: ${entries.items.length}`)
     })
     .catch((err) => {
       console.error(err)
     })
 }
 
-exec(SPACE_ID, ACCESS_TOKEN)
+exec()
